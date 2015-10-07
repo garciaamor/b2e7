e398414774d1885ba0aa4d916af2624e3ec7da13
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2015-10-07 10:52:33 +0200

    Cambio mensaje

---
+++ src/boletin2_8/Boletin2_8.java
@@ -0,0 +1,25 @@
+
+package boletin2_8;
+
+import java.util.Scanner;
+
+
+public class Boletin2_8 {
+
+   
+    public static void main(String[] args) {
+       float nom, interes, anos, t, potencia, renda;
+        Scanner obx= new Scanner(System.in);
+        System.out.println("introduce el nominal del prestamo : ");
+        nom=obx.nextFloat();
+        System.out.println ("introduce el interes anual : ");
+        interes=obx.nextFloat();
+        System.out.println("introduce el numero de años : ");
+        anos=obx.nextFloat();
+        t= anos*12;
+        potencia= (float) Math.pow((1+interes/100), t);
+        renda=(nom*(potencia)*interes/100)/(potencia-1);
+        System.out.println("La renta mensual a pagar es de : "+ renda +" euros ");
+    }
+    
+}
